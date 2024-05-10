33b52413e6e51c7265d1f4f84c1cb875bd9c1fdd
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-10 10:26:54 +0700

    ketika sekarang null

--- stackusingLinkedlist_0139/stackusingLinkedlist_0139.cpp
+++ stackusingLinkedlist_0139/stackusingLinkedlist_0139.cpp
@@ -41,6 +41,13 @@ public:
 		if (top == NULL) {
 			cout << "List is empty." << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
 
+		}
 	}
 };
